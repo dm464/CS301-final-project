79c8e8fad2d5dc57870674ff6bfdf04fcdff55ad
Ranfis Francisco <ranfis.francisco@gmail.com>
2020-11-19 20:28:14 -0500

    Add files via upload

--- final.py
+++ final.py
@@ -1,3 +1,5 @@
+# CS 301 Final Project
+
 from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
@@ -10,7 +12,7 @@ from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
 from sklearn.linear_model import LinearRegression
 
-
+# Task 1 (visualization should be in separate file?)
 data = pd.read_csv("fetal_health-1.csv")
 X = data.drop('fetal_health', axis=1)
 y = data['fetal_health']
@@ -24,3 +26,17 @@ for i in y:
         class_count[i] = 1
 
 print(class_count)
+
+# Task 2
+
+
+# Task 3
+
+
+# Task 4
+
+
+# Task 5
+
+
+# Task 6
